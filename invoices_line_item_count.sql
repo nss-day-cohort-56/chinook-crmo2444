5884407f6d79e3586747c25c11248062b5870841
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-08-09 13:25:17 -0500

    all invoice and invoiceline count

---
+++ invoices_line_item_count.sql
@@ -0,0 +1,7 @@
+/* Provide a query that shows all Invoices. The resultant table should include:
+InvoiceId
+The total number of line items on each invoice */
+
+SELECT Invoice.InvoiceId, COUNT(InvoiceLine.InvoiceId) FROM Invoice
+    JOIN InvoiceLine ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+    GROUP BY Invoice.InvoiceId
